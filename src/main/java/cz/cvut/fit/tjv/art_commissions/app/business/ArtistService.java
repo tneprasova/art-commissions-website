edca294e6d28ceb728b3b26fc85227c121abba03
Tereza Neprašová <neprater@fit.cvut.cz>
2022-10-30 21:47:21 +0100

    Add new methods to business

--- src/main/java/cz/cvut/fit/tjv/art_commissions/app/business/ArtistService.java
+++ src/main/java/cz/cvut/fit/tjv/art_commissions/app/business/ArtistService.java
@@ -1,12 +1,32 @@
 package cz.cvut.fit.tjv.art_commissions.app.business;
 
 import cz.cvut.fit.tjv.art_commissions.app.dao.ArtistRepository;
+import cz.cvut.fit.tjv.art_commissions.app.domain.ArtType;
 import cz.cvut.fit.tjv.art_commissions.app.domain.Artist;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Collection;
 
 @Service
 public class ArtistService extends AbstractCrudService<Artist, Long> {
     protected ArtistService(ArtistRepository repository) {
         super(repository);
+    }
+
+    public Collection<Artist> orderByActiveCommissionsToDate(LocalDate date) {
+        return ((ArtistRepository) repository).readAllByActiveCommissionsToDate(date);
+    }
+
+    public Collection<Artist> orderByPrice() {
+        return ((ArtistRepository) repository).readAllOrderByPricePerHour();
+    }
+
+    public Collection<Artist> filterByArtType(ArtType artType) {
+        return ((ArtistRepository) repository).readAllByArtType(artType);
+    }
+
+    public Collection<Artist> filterByName(String name) {
+        return ((ArtistRepository) repository).readAllByName(name);
     }
 }
--- src/main/java/cz/cvut/fit/tjv/art_commissions/app/business/CommissionService.java
+++ src/main/java/cz/cvut/fit/tjv/art_commissions/app/business/CommissionService.java
@@ -4,9 +4,15 @@ import cz.cvut.fit.tjv.art_commissions.app.dao.CommissionRepository;
 import cz.cvut.fit.tjv.art_commissions.app.domain.Commission;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class CommissionService extends AbstractCrudService<Commission, Long> {
     public CommissionService(CommissionRepository repository) {
         super(repository);
+    }
+
+    public Collection<Commission> findByCustomerId(long id) {
+        return ((CommissionRepository) repository).findCommissionsByCreatorId(id);
     }
 }
