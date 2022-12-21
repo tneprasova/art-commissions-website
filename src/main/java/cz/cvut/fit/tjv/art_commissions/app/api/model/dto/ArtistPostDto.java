756eb2c95dcf8fc1503dfcc7b4cd5a46eb9ea8be
Tereza Neprašová <neprater@fit.cvut.cz>
2022-12-21 02:25:49 +0100

    Add post DTOs

---
+++ src/main/java/cz/cvut/fit/tjv/art_commissions/app/api/model/dto/ArtistPostDto.java
@@ -0,0 +1,22 @@
+package cz.cvut.fit.tjv.art_commissions.app.api.model.dto;
+
+import cz.cvut.fit.tjv.art_commissions.app.domain.ArtType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ArtistPostDto {
+    private String name;
+    private int pricePerHour;
+    private ArtType artType;
+    private Collection<Long> commissions;
+    private Long teacher;
+    private Collection<Long> apprentices;
+
+    public Long getTeacher() { return this.teacher; }
+}
